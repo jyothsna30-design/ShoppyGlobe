e36de0077868afd972b5c6c2e96abb5c69bd9411
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-18 11:07:23 +0300

    modified code

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,17 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import ProductDetail from "../components/ProductDetail";
-import Cart from "../components/Cart";
-import Checkout from "../components/Checkout";
-import NotFound from "../components/NotFound";
+import ProductDetail from "../components/ProductDetail.jsx";
+import Cart from "../components/Cart.jsx";
+import Checkout from "../components/Checkout.jsx";
+import NotFound from "../components/NotFound.jsx";
+import Layout from "../components/Layout.jsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element:<Layout />,
     errorElement: <NotFound />,
+    children: [
+      {index:true, element: <App />},
+      { path: "product/:id", element: <ProductDetail /> },
+  { path: "cart", element: <Cart /> },
+  { path: "checkout", element: <Checkout /> },
+    ]
   },
-  { path: "/product/:id", element: <ProductDetail /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/checkout", element: <Checkout /> },
+  
 ]);
