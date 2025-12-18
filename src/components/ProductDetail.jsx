29cfc7cdd7cd4a77af2fa4216e33b6b61fc376a7
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-18 11:04:52 +0300

    modified details

---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { addToCart } from "../features/cart/cartSlice";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux"
+
+export default function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then(res => res.json())
+      .then(setProduct);
+  }, [id]);
+
+  if (!product) return <p>Loading...</p>;
+
+  return (
+  <div className="border-0 rounded-lg ml-130 mt-20 w-150">
+    <img className="h-70 w-70 bg-gray-300" src={product.thumbnail}></img>
+  <h2>{product.title}</h2>
+  <p className="text-xl font-semibold">${product.price}</p>
+        <button className="border-2 mt-5 rounded-lg mr-2 hover:scale-105 w-60 bg-amber-300" onClick={() => dispatch(addToCart(product))}>
+          Add to Cart
+        </button>
+        
+  </div>)
+}
