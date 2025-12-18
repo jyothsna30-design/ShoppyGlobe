8e2edbfa8a590bcf3d5884662ccc359ed4799161
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-18 11:10:03 +0300

    modified code

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
-import searchReducer from "../features/products/searchSlice";
+import searchSliceReducer from  "../features/cart/products/searchSlice"
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    search: searchReducer,
+    search: searchSliceReducer,
   },
 });
