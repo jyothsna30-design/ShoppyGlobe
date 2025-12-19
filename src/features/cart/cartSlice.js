af38d405bf5509995c809665c4fa4023d589bbe9
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:37:14 +0300

    added comments

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+//creating a cartslice
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
   },
-  reducers: {
+  reducers: {//addtocart reducer, check if the product is in cart then increment the quantity otherwise add the new product to the cart and quantity as 1.
     addToCart(state, action) {
       const item = state.items.find(i => i.id === action.payload.id);
       if (item) {
@@ -14,15 +14,19 @@ const cartSlice = createSlice({
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
+    //remove an item from the cart
     removeFromCart(state, action) {
       state.items = state.items.filter(i => i.id !== action.payload);
     },
+
+    //updates the quantity of an item
     updateQuantity(state, action) {
       const item = state.items.find(i => i.id === action.payload.id);
       if (item && action.payload.quantity >= 1) {
         item.quantity = action.payload.quantity;
       }
     },
+    //clears all the items
     clearCart(state) {
       state.items = [];
     },
