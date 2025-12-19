cbb8c483d8039061a396b091d3a6dc11b3e33e9f
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:39:22 +0300

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React, { Suspense } from "react"
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
+import React from "react"
+//LazyLoading of productlist
 const ProductList = React.lazy(() => import("./components/ProductList"));
 
 
