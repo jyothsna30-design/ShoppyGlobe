20e7de741f14f8942b1fefd68b5b4cae384ee526
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:34:44 +0300

    added comments

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header"
 
 function Layout(){
+    //returning header and outlet components
     return(<>
     <Header></Header>
     <Outlet></Outlet>
