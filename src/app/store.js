a05fcfc87500060602a510b6d592110945c00390
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:32:52 +0300

    added comments

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
 import searchSliceReducer from  "../features/cart/products/searchSlice"
 
+
+//Configuring the store with cart and search slice reducers
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
