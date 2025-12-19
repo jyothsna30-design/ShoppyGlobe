905bb5ade4ca14f75d6cabfaa5f47c7479806ca6
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:33:35 +0300

    added comments

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,9 +6,9 @@ import {
 
 
 
-export default function CartItem({ item }) {
+ function CartItem({ item }) {
   const dispatch = useDispatch();
-
+//increase quantity of a product in cart
   const increaseQty = () => {
     dispatch(
       updateQuantity({
@@ -17,7 +17,7 @@ export default function CartItem({ item }) {
       })
     );
   };
-
+ //decrease quantity of a product in cart
   const decreaseQty = () => {
     if (item.quantity > 1) {
       dispatch(
@@ -31,26 +31,26 @@ export default function CartItem({ item }) {
 
   return (
     <div className="ml-10 mt-5 flex border-2 rounded-lg">
-      {/* Product Image */}
+      {/* Product Image on lazy loading */}
       <img
         src={item.thumbnail}
         alt={item.title}
         loading="lazy" className="bg-gray-300 w-50 h-50 mr-10"
       />
 
-      {/* Product Info */}
+      {/* Product title and price */}
       <div>
         <h4 className="font-bold mt-3">{item.title}</h4>
         <p className="font-bold mt-3">Price: ${item.price}</p>
 
-        {/* Quantity Controls */}
+        {/* increment and decrement buttons */}
         <div>
           <button className="border-2 rounded-lg mr-2" onClick={decreaseQty}>−</button>
           <span>{item.quantity}</span>
           <button className="border-2 rounded-lg ml-2" onClick={increaseQty}>+</button>
         </div>
 
-        {/* Remove Button */}
+        {/* Remove from cart */}
         <button
           className="border-2 rounded-lg mt-2"
           onClick={() =>{dispatch(removeFromCart(item.id)); }}
@@ -62,3 +62,5 @@ export default function CartItem({ item }) {
     </div>
   )
 }
+
+export default CartItem
