0623f2d4e5d4a0b42327d5263034321c714fc8a9
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-19 10:36:14 +0300

    added comments

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,18 +2,19 @@ import { useSelector } from "react-redux";
 import { useFetchProducts } from "../hooks/useFetchProducts";
 import ProductItem from "./ProductItem";
 
-export default function ProductList() {
+ function ProductList() {
   const { products, loading, error } = useFetchProducts();
   const query = useSelector(state => state.search.query);
-
+  //searches if title is in products and sends the filtered array
   const filtered = products.filter(p =>
     p.title.toLowerCase().includes(query.toLowerCase())
   );
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
-
+//sending props to productitem
   return(<div className="flex flex-row flex-wrap justify-between ">
     {filtered.map(product => (<ProductItem key={product.id} product={product} />))}
   </div>);
 }
+export default ProductList;
