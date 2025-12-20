6815231c0e6423dc6e40f6530c75c362a24c4927
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-20 10:29:18 +0300

    lazy loading

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -17,7 +17,7 @@ export default function ProductDetail() {
   if (!product) return <p>Loading...</p>;
 
   return (
-  <div className="border-0 rounded-lg ml-130 mt-20 w-150">
+  <div className="border-0 rounded-lg lg:ml-130 mt-20 w-150">
     <img className="h-70 w-70 bg-gray-300" src={product.thumbnail}></img>
   <h2>{product.title}</h2>
   <p className="text-xl font-semibold">${product.price}</p>
