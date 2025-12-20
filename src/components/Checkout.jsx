22e786af6b05bb141af7d0d57a8711d962a2eacf
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2025-12-20 10:28:14 +0300

    lazy loading

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,23 +2,39 @@ import { useDispatch } from "react-redux";
 import { clearCart } from "../features/cart/cartSlice";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 
 export default function Checkout() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const items = useSelector((state) => state.cart.items || []);
+  const [inputval,setinputval] = useState("");
+  const [phone,setphoneval] = useState("");
+  const [addressval,setaddressval] = useState("");
+  const [emailval,setemailval] = useState("");
 
   const total = items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  
 //if order placed then clear items and go to home page
   const placeOrder = () => {
+    if(!inputval||!phone||!addressval||!emailval){alert("please fill the details");return;}
     alert("Order placed");
     dispatch(clearCart());
     navigate("/");
   };
   //calculate price and total amount
-  return (<>
-    <div className="ml-20 border-2 rounded-lg mt-5 w-180 p-10">
+  return (<div className="flex flex-col md:flex-row">
+    <div>
+       <h2 className="text-2xl font-bold mb-4 ml-20">Enter your Details:</h2>
+      <form className="border-2 rounded-lg w-75 md:w-100 m-2 md:ml-20 md:p-10">
+       <b> Name:</b><br></br><input type="text" className="border-2 rounded-lg m-2 w-60 mb-2" onChange={(e)=>setinputval(e.target.value)} required></input><br></br>
+        <b>Phone number:</b><br></br><input type="tel" className="border-2 rounded-lg m-2 w-60 mb-2" onChange={(e)=>setphoneval(e.target.value)} required></input><br></br>
+        <b>Email:</b><br></br><input type="email" className="border-2 rounded-lg m-2 w-60 mb-2" onChange={(e)=>setemailval(e.target.value)} required></input><br></br>
+        <b>Address:</b><br></br><textarea required className="border-2 rounded-lg m-2 w-60 h-20" onChange={(e)=>setaddressval(e.target.value)}></textarea>
+      </form>
+    </div>
+    <div className="md:ml-20 border-2 rounded-lg mt-5 md:w-120 p-10">
       <h1 className="text-2xl font-bold mb-4">Checkout</h1>
       {items.map(item => (
         <div key={item.id} className="flex justify-between">
@@ -32,6 +48,6 @@ export default function Checkout() {
       </button>
     </div>
     
-    </>
+    </div>
   );
 }
